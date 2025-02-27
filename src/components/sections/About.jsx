161261d679162c04d3d766a0699fe2db533e5b5c
Winston Malcolm <101194070+winstonmalcolm-tech@users.noreply.github.com>
2025-02-27 18:19:46 -0500

    Update About.jsx

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
             <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center'>About Me</h2>
 
             <div className='rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all'>
-                <p className='text-gray-300 mb-6'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio rerum ex magnam consequuntur saepe esse in atque facilis distinctio, quam provident quasi libero ullam debitis consequatur eius. Sint, molestiae velit quas perspiciatis dicta harum autem repellat maxime qui repellendus totam, obcaecati unde illum pariatur! Totam error laborum obcaecati a. Dolores eum aspernatur sint illo commodi animi quod dolor, possimus delectus voluptate, sunt id velit!</p>
+                <p className='text-gray-300 mb-6'>Motivated and detail-oriented software developer with a strong foundation in programming languages such as Dart and JavaScript. Skilled in building projects through self-directed learning and online courses, I have developed a passion for creating innovative software solutions. With hands-on experience from apprenticeship and personal projects, I thrive in collaborative environments and enjoy tackling challenges with creative problem-solving. Eager to contribute to a dynamic team, I am committed to continuous learning and staying updated with the latest industry trends to help develop impactful software that enhances user experiences.</p>
 
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
                     <div className='rounded-xl p-6 hover:-translate-y-1 translate-all'>
