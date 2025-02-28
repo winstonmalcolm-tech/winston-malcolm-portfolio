50200559a2e1ef3d0f74e462ce298dd4e3c59d18
Winston Malcolm <winstionmalcolm77@gmail.com>
2025-02-27 19:07:40 -0500

    fix navbar

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -9,7 +9,7 @@ const MobileMenu = ({menuOpen, setMenuOpen}) => {
 
 
   return (
-    <div className={`fixed top-0 left-0 w-full bg-(10,10,10,0.8) z-40 flex flex-col items-center justify-center transition-all duration-300 ease-in-out ${menuOpen ? "h-screen opacity-100 pointer-events-auto" : "h-0 opacity-0 pointer-events-none"}`}>
+    <div className={`fixed top-0 left-0 w-full bg-[rgba(10,10,10,0.8)] z-40 flex flex-col items-center justify-center transition-all duration-300 ease-in-out ${menuOpen ? "h-screen opacity-100 pointer-events-auto" : "h-0 opacity-0 pointer-events-none"}`}>
         <button aria-label='Close Menu' onClick={() => setMenuOpen(false)} className='absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer'>
             &times;
         </button>
