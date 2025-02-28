b81ac977d8d14ab41d909d9e5fb5209051e2559d
Winston Malcolm <winstionmalcolm77@gmail.com>
2025-02-28 11:33:26 -0500

    fix email feature

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Navigate, BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to={"/winston-malcolm-portfolio"} replace/>} />
-        <Route path='/winston-malcolm-portfolio' element={<App />} />
-        <Route path="*" element={<Navigate to={"/winston-malcolm-portfolio"} replace/>} />
-      </Routes>
-      
-    </Router>
+    <App />
   </StrictMode>,
 )
