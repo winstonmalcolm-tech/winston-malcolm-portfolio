d7ed595df3dd0f3e45fa726df74eccc2d8d947e5
Winston Malcolm <programmermalx27@gmail.com>
2025-04-15 16:23:32 -0500

    Add resume

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
 import RevealOnScroll from "../RevealOnScroll";
+import Resume from "../../assets/winston_malcolm_resume.docx";
 
 const Home = () => {
   return (
     <section id='home' className='min-h-screen flex items-center justify-center relative'>
         <RevealOnScroll>
           <div className='text-center z-10 px-4'>
-              <h1 className='text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right'>Hi, I'm Winston Malcolm</h1>
+              <h1 className='text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right'>Hi, I&apos;m Winston Malcolm</h1>
               <p className='text-gray-400 text-lg mb-8 max-w-lg mx-auto'>
                   I am a software developer that specializes in building both mobile and web applications, ensuring seamless functionality and user-friendly designs across all platforms.
               </p>
 
               <div className='flex justify-center space-x-4'>
                   <a href="#projects" className='bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]'>View Projects</a>
-                  <a href="#contact" className='border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10'>Contact Me</a>
+                  <a href={Resume} className='border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10'>Resume</a>
               </div>
           </div>
         </RevealOnScroll>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  assetsInclude: ["**/*.docx"],
   plugins: [
     react(),
     tailwindcss(),
